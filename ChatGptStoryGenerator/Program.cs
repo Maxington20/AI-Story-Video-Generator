0dd26432f8d1b080275d41be5baa51a5f40de293
Maxington20 <max.herrington@gmail.com>
2023-03-20 16:12:12 -0400

    Add project files.

---
+++ ChatGptStoryGenerator/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using System.Security.Principal;
+//using System.Speech.Synthesis;
+using Microsoft.CognitiveServices.Speech;
+
+class Program
+{
+    public class DataBody
+    {
+        public string model { get; set; }
+        public List<Message> messages { get; set; }
+    }
+
+    public class Message
+    {
+        public string role { get; set; }
+        public string content { get; set; }
+    }
+
+    static async Task Main(string[] args)
+    {
+        // List of topics to choose from
+        List<string> topics = new List<string>
+        {
+            "cat", "dog", "ferret", "chinchilla", "pickle", "tomato", "fish", "octopus", "alligator", "snail", "bumblebee", "shark", "whale", "sloth"
+        };
+
+        // Choose a random topic from the list
+        Random rand = new Random();
+        string topic = topics[rand.Next(topics.Count)];
+
+        // Use Google Custom Search API to search for images related to the topic
+        string apiKey = "Bearer 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";
+        string url = "https://api.openai.com/v1/chat/completions";
+
+        using (var httpClient = new HttpClient())
+        {
+            httpClient.DefaultRequestHeaders.Add("Authorization", apiKey);
+
+            var body = new DataBody { model = "gpt-3.5-turbo", messages = new List<Message>() };
+
+            body.messages.Add(new Message { role = "user", content = $"create a ~300 word children's story about {topic}" });
+
+            string json = JsonConvert.SerializeObject(body);
+
+            HttpContent content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+            Console.Write("Now hitting the chat gpt api");
+            var response = await httpClient.PostAsync(url, content);
+
+            if(response.StatusCode == HttpStatusCode.OK)
+            {
+                Console.WriteLine("Woohoo we got a 200 response");
+            }
+
+            var storyContent = await response.Content.ReadAsStringAsync();
+
+            // Parse the JSON response to get the image URLs
+            dynamic result = JsonConvert.DeserializeObject(storyContent);
+
+            string story = result?.choices[0]?.message?.content ?? "There was some kind of error";
+
+            Console.WriteLine(story);
+
+            var  speechKey = "f5c6b2a2ee8343a6a2a4adf91b8d83c9";
+            var  regionKey = "eastus";
+
+            var speechConfig = SpeechConfig.FromSubscription(speechKey, regionKey);
+
+            speechConfig.SpeechSynthesisVoiceName = "en-US-AnaNeural";
+
+            using (var speechSynthesizer = new SpeechSynthesizer(speechConfig))
+            {
+                var speechResult = await speechSynthesizer.SpeakTextAsync(story);
+
+                Console.WriteLine("Saving the audio to a file");
+                using var stream = AudioDataStream.FromResult(speechResult);
+                await stream.SaveToWaveFileAsync($"C:\\Users\\maxhe\\OneDrive\\Pictures\\Saved Pictures\\StoryLogs\\{topic}.wav");
+            }        
+
+            // track the list of used animals and the date/time associated with each            
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\maxhe\\OneDrive\\Pictures\\Saved Pictures\\StoryLogs\\animallog.txt", append: true))
+            {             
+                writer.WriteLine($"{topic} - {DateTime.Now}");
+            }
+
+            // track the most recent story
+            using (StreamWriter writer = new StreamWriter("C:\\Users\\maxhe\\OneDrive\\Pictures\\Saved Pictures\\StoryLogs\\storylog.txt"))
+            {
+                writer.WriteLine(story);
+            }
+        }
+
+        Console.WriteLine("Done!");
+    }
+}
---
+++ ImageDownloader/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // List of topics to choose from
+        List<string> topics = new List<string>
+        {
+            "cat", "dog", "ferret", "chinchilla", "pickle"
+        };
+
+        // Choose a random topic from the list
+        Random rand = new Random();
+        string topic = topics[rand.Next(topics.Count)];
+
+        // Use Google Custom Search API to search for images related to the topic
+        string apiKey = "AIzaSyDMg_4iX_gQd9E7WksgsWdfLMT2D4w9o-I";
+        string cx = "11764fb6d23c841e5";
+        string imgType = "stock";
+        string url = $"https://www.googleapis.com/customsearch/v1?q=cartoon%20{topic}&cx={cx}&key={apiKey}&searchType=image&imgType={imgType}";     
+
+        using (var httpClient = new HttpClient())
+        {
+            var response = await httpClient.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Parse the JSON response to get the image URLs
+            dynamic result = JsonConvert.DeserializeObject(content);
+            List<string> imageUrls = new List<string>();
+            foreach (var item in result.items)
+            {
+                imageUrls.Add(item.link.ToString());
+            }
+
+            // Download the images to a local directory
+            string downloadPath = "C:\\Users\\maxhe\\OneDrive\\Pictures\\Saved Pictures\\AutomationImages";
+            int count = 1;
+            foreach (string imageUrl in imageUrls)
+            {
+                using (var imageResponse = await httpClient.GetAsync(imageUrl))
+                {
+                    var imageContent = await imageResponse.Content.ReadAsStreamAsync();
+                    var fileName = $"{topic}_{count}.jpg";
+                    using (var fileStream = new FileStream(Path.Combine(downloadPath, fileName), FileMode.Create))
+                    {
+                        await imageContent.CopyToAsync(fileStream);
+                    }
+                    Console.WriteLine($"Downloaded image: {fileName}");
+                }
+                count++;
+            }
+        }
+
+        Console.WriteLine("Done!");
+    }
+}
