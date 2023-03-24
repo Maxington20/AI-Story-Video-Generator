45362950c7bc0ecdc78462a63f95e0cb6e3e3b7d
Maxington20 <max.herrington@gmail.com>
2023-03-24 09:04:38 -0400

    all good under the hood

--- ChatGptStoryGenerator/ChatGPTRequest.cs
+++ ChatGptStoryGenerator/ChatGPTRequest.cs
@@ -1,16 +1,43 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using static Program;
 
 namespace ChatGptStoryGenerator
 {
     public static class ChatGPTRequest
-    {
-        public static async void MakeChatGPTRequest(string content)
-        {
+    {        
 
+        public static async Task<string> MakeChatGPTRequest(string apiKey,string url,string searchContent)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("Authorization", apiKey);
+
+                httpClient.Timeout = Timeout.InfiniteTimeSpan;
+
+                var body = new DataBody { model = "gpt-3.5-turbo", messages = new List<Message>() };
+
+                body.messages.Add(new Message { role = "user", content = searchContent });
+
+                string json = JsonConvert.SerializeObject(body);
+
+                HttpContent content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+                Console.Write("Now hitting the chat gpt api");
+                var response = await httpClient.PostAsync(url, content);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Console.WriteLine("\n\nWoohoo we got a 200 response");
+                }
+
+                 return await response.Content.ReadAsStringAsync();
+            }
         }
     }
 }
